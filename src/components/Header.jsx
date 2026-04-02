e2ab0b7bbb467857d9c21a2c3637dcf276100175
Allen Rakhamimov <97687960+ARakhamimov@users.noreply.github.com>
2026-04-01 20:37:34 -0400

    flex wrap changes
    added flex wraps to make it a little better on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-      <div className="bg-zinc-50 shadow-sm shadow-red-400 flex justify-between sticky top-0 z-50">
+      <div className="bg-zinc-50 shadow-sm shadow-red-400 flex justify-between sticky top-0 z-50 lg:flex-nowrap flex-wrap">
         <h1 className="text-4xl text-red-600 hover:text-red-800 font-serif font-bold content-center py-6 px-12">
             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"> Allen Rakhamimov</a>
         </h1>
